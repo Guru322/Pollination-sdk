f74d893ea8b7bca442f72a278361b84be17731a1
GURU SENSEI <sahoogurucharan00@gmail.com>
2025-01-14 17:45:38 +0530

    Create validator.js

---
+++ validator.js
@@ -0,0 +1,26 @@
+class Validator {
+  static validateString(value, fieldName) {
+    if (typeof value !== 'string' || !value.trim()) {
+      throw new Error(`${fieldName} must be a non-empty string`);
+    }
+    return value.trim();
+  }
+
+  static validateNumber(value, fieldName, min, max, defaultValue) {
+    if (value === undefined) {
+      return defaultValue;
+    }
+    const num = Number(value);
+    if (isNaN(num) || num < min || num > max) {
+      throw new Error(`${fieldName} must be a number between ${min} and ${max}`);
+    }
+    return num;
+  }
+
+  static validateBoolean(value, fieldName, defaultValue) {
+    if (value === undefined) {
+      return defaultValue;
+    }
+    return Boolean(value);
+  }
+}
